ae7cd3360991ef9bcc7e53f1d2fe0e163de06dd8
SRIRAM <srirammurugesan1807@gmail.com>
2025-06-27 21:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your code here
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script>
+    const form = document.getElementById('internshipForm');
+    form.addEventListener('submit', function(event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    });
